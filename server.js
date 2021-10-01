549512c702c0d59335400618e4a9f7598630b252
KaranSurana <techtrends288@gmail.com>
2021-10-01 17:45:06 +0530

    Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const path = require('path');
+
+// defining port
+const port = 8080;
+
+const app = express();
+
+// setting up monogdb
+const db = require('./config/mongoose')
+
+const repository = require('./models/product');
+
+app.use(express.urlencoded());
+
+app.use('/',require('./routes/index'));
+
+// express listening to the port and returning
+app.listen(port,function(err){
+    if(err){
+        console.log("Error In Running The Server!");
+        return;
+    }
+    console.log("Server Running!");
+})
